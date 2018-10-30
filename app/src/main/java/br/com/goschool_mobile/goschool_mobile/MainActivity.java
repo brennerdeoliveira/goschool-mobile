2f0b3e510c488eeb12f660e623bf619ebcdb6daa
Vicente <vicente-psp@hotmail.com>
2018-10-30 19:53:14 -0300

    commite inicial

---
+++ app/src/main/java/br/com/goschool_mobile/goschool_mobile/MainActivity.java
@@ -0,0 +1,17 @@
+package br.com.goschool_mobile.goschool_mobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+//    private static final int ERROR
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
